8a65e99ac8045f452e5183c86bc853250749fce0
colorindarkness <urielballinas@gmail.com>
2022-06-27 18:23:06 -0700

    Fix to Batch file

--- EOS_Testing.bat
+++ EOS_Testing.bat
@@ -1,3 +1,2 @@
-set ue5path="D:\Dev\UE5\UE_5.0\Engine\Binaries\Win64\"
-echo %ue5path%
-start "" %ue5path%"UnrealEditor.exe" %~dp0"EOSLearning.uproject" -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log
+set ue5path=D:\Dev\UE5\UE_5.0\Engine\Binaries\Win64\
+start "" %ue5path%UnrealEditor.exe %~dp0EOSLearning.uproject -game -ResX=800 -ResY=900 -WinX=0 -WinY=20 -log
