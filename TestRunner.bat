97b43dc72393d9b1dd88d31bb5e3bd4e2e0bb584
colorindarkness <urielballinas@gmail.com>
2022-07-14 05:12:28 -0700

    Fix to batch file

--- TestRunner.bat
+++ TestRunner.bat
@@ -5,4 +5,4 @@ set testSuiteToRun=%~4
 set testReportFolder=%~5
 set testsLogName=%~6
 
-"%ue5Location%\Engine\Binaries\Win64\UnrealEditor-cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests %testSuiteToRun%;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -Log=%testsLogName%
+"%ue5Location%\Engine\Binaries\Win64\UnrealEditor-cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests %testSuiteToRun%;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -Log="%workspace%\Saved\TestLogs\%testsLogName%"
