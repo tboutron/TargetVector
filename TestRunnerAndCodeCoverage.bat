32176e011fb15bf36f4494e0772887e04a4f3b72
colorindarkness <urielballinas@gmail.com>
2022-07-15 17:02:34 -0700

    fix typo

--- TestRunnerAndCodeCoverage.bat
+++ TestRunnerAndCodeCoverage.bat
@@ -8,4 +8,4 @@ set codeCoverageFile=%~7
 
 set testRunnerCommand="%ue5Location%\Engine\Binaries\Win64\UnrealEditor-cmd.exe" "%workspace%\%projectFilename%" -nosplash -Unattended -nopause -nosound -NullRHI -nocontentbrowser -ExecCmds="Automation RunTests %testSuiteToRun%;quit" -TestExit="Automation Test Queue Empty" -ReportOutputPath="%workspace%\%testReportFolder%" -Log=%testsLogName%
 
-"C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" --sources=%workspace%\Source --modules %workspace% --excluded_sources=\Tests --export_type=cobertura:%testReportFolder%\%codeCoverageFile%  -- %testRunnerCommand%
+"C:\Program Files\OpenCppCoverage\OpenCppCoverage.exe" --sources=%workspace%\Source --modules %workspace% --excluded_sources=\Tests --export_type=cobertura:%codeCoverageFile%  -- %testRunnerCommand%
