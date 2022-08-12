b8340f4313d5386bba5285579a088e38b66836e9
colorindarkness <urielballinas@gmail.com>
2022-08-12 03:47:34 -0700

    Remove ignored files

--- Plugins/CommonLoadingScreen/Source/CommonLoadingScreen/Private/CommonLoadingScreenSettings.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CommonLoadingScreenSettings.h"
-
-UCommonLoadingScreenSettings::UCommonLoadingScreenSettings()
-{
-	CategoryName = TEXT("Game");
-}
